32e6cedd1c14d3908d9598adad16ed3ed554f4fa
phillin77 <phillin77@gmail.com>
2019-08-18 19:13:47 +0800

    add postinstall with NODE_ENV=prod

--- dist/src/server/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = function (req, res, next) {
-    res.sendStatus(200);
-};
-//# sourceMappingURL=index.js.map
